fd3a1ada4bb47a35c0ebb715d69a2c63a47f98f7
Vitalii_Rudenko <Vitalii_Rudenko@epam.com>
2018-01-29 15:55:45 +0200

    Some basic structure

---
+++ XamarinCata/XamarinCata.Core.Unittests/XamarinCata.Core.Unittests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XamarinCata.Core.Unittests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
